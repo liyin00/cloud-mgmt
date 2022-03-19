1e6d35d363e2b2270ee40bc72a8c9f182d0c8d0e
teohjiaxing1 <79452234+teohjiaxing1@users.noreply.github.com>
2022-03-19 18:32:24 +0800

    feat BE: update naming convention

--- app/product_service/product.py
+++ app/product_service/product.py
@@ -17,16 +17,16 @@ app = Flask(__name__)
 def service_acc_conversion():
     service_account_info = {"credential":
     {
-        "type" :os.getenv("type"),
-        "project_id" : os.getenv("project_id"),
-        "private_key_id" : os.getenv("private_key_id"),
-        "private_key" : os.getenv("private_key").replace('\\n','\n'),
-        "client_email" : os.getenv("client_email"),
-        "client_id" : os.getenv("client_id"),
-        "auth_uri" : os.getenv("auth_uri"),
-        "token_uri" : os.getenv("token_uri"),
-        "auth_provider_x509_cert_url" : os.getenv("auth_provider_x509_cert_url"),
-        "client_x509_cert_url" : os.getenv("client_x509_cert_url")
+        "type" :os.getenv("TYPE"),
+        "project_id" : os.getenv("PROJECT_ID"),
+        "private_key_id" : os.getenv("PRIVATE_KEY_ID"),
+        "private_key" : os.getenv("PRIVATE_KEY").replace('\\n','\n'),
+        "client_email" : os.getenv("CLIENT_EMAIL"),
+        "client_id" : os.getenv("CLIENT_ID"),
+        "auth_uri" : os.getenv("AUTH_URI"),
+        "token_uri" : os.getenv("TOKEN_URI"),
+        "auth_provider_x509_cert_url" : os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url" : os.getenv("CLIENT_X509_CRET_URL")
     }}
     return service_account_info
 
