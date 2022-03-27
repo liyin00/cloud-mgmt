f87ff580dedaaf758dbe615a628ebd0ca670c49e
liyin00 <73151500+liyin00@users.noreply.github.com>
2022-03-27 21:52:38 +0800

    amendment of environment

--- app/product_service/product.py
+++ app/product_service/product.py
@@ -17,19 +17,22 @@ app = Flask(__name__)
 CORS(app)
 
 def service_acc_conversion():
-    service_account_info = {"credential":
-    {
-        "type" :os.getenv("TYPE"),
-        "project_id" : os.getenv("PROJECT_ID"),
-        "private_key_id" : os.getenv("PRIVATE_KEY_ID"),
-        "private_key" : os.getenv("PRIVATE_KEY").replace('\\n','\n'),
-        "client_email" : os.getenv("CLIENT_EMAIL"),
-        "client_id" : os.getenv("CLIENT_ID"),
-        "auth_uri" : os.getenv("AUTH_URI"),
-        "token_uri" : os.getenv("TOKEN_URI"),
-        "auth_provider_x509_cert_url" : os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-        "client_x509_cert_url" : os.getenv("CLIENT_X509_CERT_URL")
-    }}
+    service_account_info = {
+        "credential": os.getenv("SA_KEY")
+    }
+    # {"credential":
+    # {
+    #     "type" :os.getenv("TYPE"),
+    #     "project_id" : os.getenv("PROJECT_ID"),
+    #     "private_key_id" : os.getenv("PRIVATE_KEY_ID"),
+    #     "private_key" : os.getenv("PRIVATE_KEY").replace('\\n','\n'),
+    #     "client_email" : os.getenv("CLIENT_EMAIL"),
+    #     "client_id" : os.getenv("CLIENT_ID"),
+    #     "auth_uri" : os.getenv("AUTH_URI"),
+    #     "token_uri" : os.getenv("TOKEN_URI"),
+    #     "auth_provider_x509_cert_url" : os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    #     "client_x509_cert_url" : os.getenv("CLIENT_X509_CERT_URL")
+    # }}
     return service_account_info
 
 service_info = service_acc_conversion()
